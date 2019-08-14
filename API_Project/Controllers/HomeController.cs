ca441246811a129e63e8c4022a6639eefcfbe7a5
MadisonMott <Madison.Mott@ymail.com>
2019-08-14 12:07:42 -0400

    adding api project

---
+++ API_Project/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API_Project.Models;
+using System.Net.Http;
+
+namespace API_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://www.omdbapi.com/");
+
+            var response = await client.GetAsync("?t=detective+pikachu");
+            var content = await response.Content.ReadAsAsync<Movie>();
+            return View(content);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ API_Project/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Project.Models
+{
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+
+    }
+
+    //public class Rating
+    //{
+    //    public string Source { get; set; }
+    //    public string Value { get; set; }
+    //}
+
+}
