40838b3b8e7e071c73ac6879f931d374c2a09779
MadisonMott <Madison.Mott@ymail.com>
2019-08-16 15:37:55 -0400

    Created FavoriteMovie View & updates on controller

--- API_Project/Data/ApplicationDbContext.cs
+++ API_Project/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using API_Project.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,10 +8,12 @@ using System.Threading.Tasks;
 
 namespace API_Project.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         { }
+
+        public DbSet<Movie> FavoriteMovies { get; set; }
     }
 }
--- API_Project/Models/Movie.cs
+++ API_Project/Models/Movie.cs
@@ -8,6 +8,7 @@ namespace API_Project.Models
 
     public class Movie
     {
+        public int MovieId { get; set; }
         public string Title { get; set; }
         public string Year { get; set; }
         public string Genre { get; set; }
